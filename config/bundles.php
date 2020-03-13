40955a1901f8fab50853495abe745a4506fc1fd0
Zygimantas Ulevicius <z.ulevicius@gmail.com>
2020-03-13 20:00:33 +0200

    configs updated

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class  => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
